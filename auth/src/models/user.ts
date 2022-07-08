4294920450b286b6d768795e4508e2fcbef0971d
reinhartbuit <buit.reinhart@gmail.com>
2022-07-08 07:28:51 -0700

    Test

---
+++ auth/src/models/user.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import { Password } from '../services/password';
+
+// Interface describe properties for User
+interface userAttrs {
+  email: string;
+  password: string;
+}
+// Interface describe properties for User Model
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: userAttrs): UserDoc;
+}
+
+//Interface describes the properties for User Document
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+},{
+  toJSON: {
+    transform(doc,ret){
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.password;
+      delete ret.__v;
+    }
+  }
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: userAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };

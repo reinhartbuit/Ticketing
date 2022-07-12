fb5567a1bfcf45ea3f5698325e4fc56b9290528d
reinhartbuit <buit.reinhart@gmail.com>
2022-07-12 08:04:10 +0200

    Complete React Signout

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,6 +9,7 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async (context) => {
+  console.log('landing ');
   const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser');
   return data;

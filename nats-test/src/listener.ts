fbf1ed6c8bbd473e955a021c7b05e5f7ecff92f6
reinhartbuit <buit.reinhart@gmail.com>
2022-07-15 16:54:55 +0200

    Moved NATS to Common Directory

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -13,25 +14,7 @@ stan.on('connect', () => {
     console.log('NATS connection closed');
     process.exit();
   });
-
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service');
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'queue-group-name',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-    if (typeof data === 'string') {
-      console.log(`Received event # ${msg.getSequence()}, with data: ${data}`);
-    }
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());

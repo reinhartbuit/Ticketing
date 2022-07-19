548c968f9047bee72694bd383c7a939e5cf2ac8d
reinhartbuit <buit.reinhart@gmail.com>
2022-07-19 17:32:22 +0200

    Complete the Expiration Service

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -9,7 +9,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
     console.log('Waiting this many miliseconds to process the job', delay);
-    await expirationQueue.add({ orderId: data.id }, { delay });
+    await expirationQueue.add({ orderId: data.id }, { delay});
     msg.ack();
   }
 }

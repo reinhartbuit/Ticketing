d9a4ce28451eeccebeb6b4d27d8e376b0b0ab025
reinhartbuit <buit.reinhart@gmail.com>
2022-07-20 19:51:15 +0200

    Payments Service with Stripe

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -5,7 +5,7 @@ import request from 'supertest';
 import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
 jest.mock('../nats-wrapper');
@@ -31,10 +31,10 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
   //Building JWT payload. {id,email}
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 

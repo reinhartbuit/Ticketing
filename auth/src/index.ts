1c595d0197af6c9838e1926845d5fbd71010400d
reinhartbuit <buit.reinhart@gmail.com>
2022-07-21 22:25:50 +0200

    Test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,6 +8,7 @@ const start = async () => {
     throw new Error('MONGO_URI must be defined');
   }
   try {
+    console.log('test');
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
